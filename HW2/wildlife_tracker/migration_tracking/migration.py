36a1b738b81992a0c09ffc4264bb6db0d625e45d
jmvm1304 <jmvilla@bu.edu>
2024-12-09 08:13:42 -0500

    migration module

--- HW2/wildlife_tracker/migration_tracking/migration.py
+++ HW2/wildlife_tracker/migration_tracking/migration.py
@@ -1,5 +1,19 @@
 from typing import Any
-
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath
 class Migration:
 
-    pass
+    def __init__(self, current_date:str, current_location: str, migration_id: int, migration_path: MigrationPath, start_date:str, status: str = "Schedule") -> None:
+        self.current_date = current_date
+        self.current_location = current_location
+        self.migration_id = migration_id
+        self.migration_path = migration_path
+        self.start_date = start_date
+        self.status = status
+
+    def get_migration_details(self, migration_id: int) -> dict[str, Any]:
+        pass
+    def schedule_migration(self, migration_path: MigrationPath) -> None:
+        pass
+    def update_migration_details(self, migration_id: int, **kwargs: Any) -> None:
+        pass
+
--- HW2/wildlife_tracker/migration_tracking/migration_path.py
+++ HW2/wildlife_tracker/migration_tracking/migration_path.py
@@ -1,5 +1,16 @@
 from typing import Optional
+from wildlife_tracker.habitat_manager.habitat import Habitat
 
 class MigrationPath:
+    def __init__(self, destination: Habitat, duration: Optional[int] = None, path_id: int, species: str, start_location: Habitat) ->None: 
+        self.destination = destination
+        self.duration = duration
+        self.path_id = path_id
+        self.species = species
+        self.start_location = start_location
 
-    pass
+    def get_migration_path_details(self,path_id: int) -> dict:
+        pass
+
+    def update_migration_path_details(self, path_id: int, **kwargs) -> None:
+        pass
