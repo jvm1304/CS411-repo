fca368444e6fb15b771a7d84730ba0a3480d075d
jmvm1304 <jmvilla@bu.edu>
2024-12-09 07:22:32 -0500

    animal_management done

--- HW2/wildlife_tracker/animal_management/animal.py
+++ HW2/wildlife_tracker/animal_management/animal.py
@@ -1,5 +1,14 @@
 from typing import Any, Optional
 
 class Animal:
+    def __init__(self, age: Optional[int] = None, animal_id: int, health_status: Optional[str] = None, species: str) -> None:
+        self.age = age
+        self.animal_id = animal_id
+        self.health_status = health_status
+        self.species = species
 
-    pass
+    def get_animal_details(animal_id) -> dict[str, Any]:
+        pass
+
+    def update_animal_details(self, **kwargs: Any) -> None:
+        pass
