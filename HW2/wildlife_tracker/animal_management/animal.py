93f8d8ff736d8897e8a78ed8f83ad5b9453aa881
jmvm1304 <jmvilla@bu.edu>
2024-12-09 09:26:43 -0500

    passes import test

--- HW2/wildlife_tracker/animal_management/animal.py
+++ HW2/wildlife_tracker/animal_management/animal.py
@@ -1,7 +1,7 @@
 from typing import Any, Optional
 
 class Animal:
-    def __init__(self, age: Optional[int] = None, animal_id: int, health_status: Optional[str] = None, species: str) -> None:
+    def __init__(self, animal_id: int, species: str, age: Optional[int] = None, health_status: Optional[str] = None) -> None:
         self.age = age
         self.animal_id = animal_id
         self.health_status = health_status
--- HW2/wildlife_tracker/migration_tracking/migration_path.py
+++ HW2/wildlife_tracker/migration_tracking/migration_path.py
@@ -2,7 +2,7 @@ from typing import Optional
 from wildlife_tracker.habitat_manager.habitat import Habitat
 
 class MigrationPath:
-    def __init__(self, destination: Habitat, duration: Optional[int] = None, path_id: int, species: str, start_location: Habitat) ->None: 
+    def __init__(self, destination: Habitat, path_id: int, species: str, start_location: Habitat, duration: Optional[int] = None) ->None: 
         self.destination = destination
         self.duration = duration
         self.path_id = path_id
