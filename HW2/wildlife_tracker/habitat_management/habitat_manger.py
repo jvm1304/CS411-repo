8567b9e90f57ad8ca69b18e801861f8825091499
jmvm1304 <jmvilla@bu.edu>
2024-12-09 07:34:37 -0500

    habitat_management

--- HW2/wildlife_tracker/habitat_management/habitat_manger.py
+++ HW2/wildlife_tracker/habitat_management/habitat_manger.py
@@ -1,5 +1,28 @@
 from typing import Optional, List
+from wildlife_tracker.animal.animal import Animal
+from wildlife_tracker.habitat_manager.habitat import Habitat
 
 class HabitatManager:
 
-    pass
+    def __init__(self) -> None:
+        self.habitats: dict[int, Habitat] = {}
+
+    def assign_animals_to_habitat(habitat_id: int, animals: List[Animal]) -> None:
+        pass
+
+    def create_habitat(habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
+        pass
+
+    def get_habitat_by_id(habitat_id: int) -> Habitat:
+        pass
+
+    def get_habitats_by_geographic_area(geographic_area: str) -> List[Habitat]:
+        pass
+
+    def get_habitats_by_size(size: int) -> List[Habitat]:
+        pass
+
+    def get_habitats_by_type(environment_type: str) -> List[Habitat]:
+        pass
+    def remove_habitat(habitat_id: int) -> None:
+        pass
